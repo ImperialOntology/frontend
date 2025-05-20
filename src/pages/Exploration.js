838bc184e6d106c72f32e4afba8b98f780671df9
Lingjun Gao <lg524@imperial.ac.uk>
2025-05-20 12:30:28 +0100

    test complete

--- src/pages/Exploration.js
+++ src/pages/Exploration.js
@@ -5,7 +5,6 @@ import CardItem from "../components/CardItem";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = process.env.REACT_APP_BACKEND_URL || "http://131.145.94.64:8000";
-const KEY = process.env.REACT_APP_TEST_KEY;
 
 function Exploration() {
   const [dataSources, setDataSources] = useState([]);
